0c00c25abdeba0d41151baa475e084962ec6c3db
Corey <seschwan@yahoo.com>
2020-07-16 21:55:13 -0600

    Finished word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,21 @@
 def word_count(s):
     # Your code here
 
+    string_list = {}
+    lower_string = s.lower()
+    ignored_chars = '" : ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(" ")
+
+    for char in ignored_chars:
+        lower_string = lower_string.replace(char, "")
+
+    for word in lower_string.split():
+        if word == "":
+            continue
+        if word not in string_list:
+            string_list[word] = 1
+        else:
+            string_list[word] += 1
+    return string_list
 
 
 if __name__ == "__main__":
